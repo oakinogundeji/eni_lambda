aac09946a8b5587be768245409ba72a6fe77c531
telios <oakinogundeji@gmail.com>
2019-04-22 06:22:31 +0100

    :tada: lambda function bound to AWS Autoscaling lifecycles to auto attach ENI to EC2 instance

--- eni.js
+++ eni.js
@@ -2,8 +2,7 @@
 
 const
   Promise = require('bluebird'),
-  AWS = require('aws-sdk');
-  //EC2 = require('aws-sdk/ec2');
+  AWS = require('aws-sdk');;
 
 AWS.config.update({
   region: 'ca-central-1'
@@ -11,11 +10,85 @@ AWS.config.update({
 
 AWS.config.setPromisesDependency(Promise);
 
-const  ec2 = new AWS.EC2 ();
+const
+  ec2 = new AWS.EC2 (),
+  autoscaling = new AWS.AutoScaling();
 
-exports.default = async (event, ctc, cbk) => {
-  const eniParams = {};
+exports.default = async (event, context, cbk) => {
+  // get instanceID and asgToken
+  const
+    newInstanceID = event["detail"]["EC2InstanceId"],
+    asgToken = event["detail"]["LifecycleActionToken"];
+  //look for available ENIs
+  const eniParams = {
+    Filters: [
+      {
+        Name: 'status',
+        Values: [
+          'available'
+        ]
+      }
+    ]
+  };
   const enis = await ec2.describeNetworkInterfaces(eniParams).promise();
-  console.log('enis');
-  return console.log(enis);
+  console.log('all enis');
+  console.log(enis);
+
+  const instanceParams = {
+    InstanceIds: [`${newInstanceID}`]
+  };
+  const newInstanceDetails = await ec2.describeInstances(instanceParams).promise();
+  console.log('newInstanceDetails');
+  console.log(JSON.stringify(newInstanceDetails));
+
+  const
+    targetAZ = newInstanceDetails["Reservations"][0]["Instances"][0]["Placement"]["AvailabilityZone"],
+    targetSubnet = newInstanceDetails["Reservations"][0]["Instances"][0]["SubnetId"];
+
+  console.log(`targetAZ: ${targetAZ}, targetSubnet: ${targetSubnet}`);
+
+  // ensure the right ENI is selected
+
+  const gotMatch = enis["NetworkInterfaces"].filter(eni => eni.AvailabilityZone == targetAZ && eni.SubnetId == targetSubnet);
+  if(!!gotMatch && gotMatch[0]["Description"].includes('data-node')) {
+    console.log('gotMatch');
+    console.log(gotMatch);
+
+    // attach ENI to new instance
+    const attachENIparams = {
+      DeviceIndex: 1,
+      InstanceId: `${newInstanceID}`,
+      NetworkInterfaceId: `${gotMatch[0].NetworkInterfaceId}`
+     };
+     console.log(`gotMatch[0].NetworkInterfaceId: ${gotMatch[0].NetworkInterfaceId}`);
+     const attachedENI = await ec2.attachNetworkInterface(attachENIparams).promise();
+     console.log('attachedENI');
+     console.log(attachedENI);
+
+     // allow ASG to continue
+
+     const asgParams = {
+       AutoScalingGroupName: 'dummy-lc-asg',
+       LifecycleActionResult: 'CONTINUE',
+       LifecycleHookName: 'data-node-up-hook',
+       LifecycleActionToken: `${asgToken}`
+     };
+     const continueASG = await autoscaling.completeLifecycleAction(asgParams).promise();
+     console.log('continueASG');
+     console.log(continueASG);
+
+     // return
+
+     const output = {
+       statusCode: 200,
+       body: JSON.stringify({
+         message: 'Successfully attached ENI to new instance via ASG lifecycle hooks',
+         input: event,
+         enis,
+         newInstanceDetails
+       }),
+     };
+     return output;
+  }
+  return null;
 };
